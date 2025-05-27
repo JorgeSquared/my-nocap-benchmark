1e092bda2b7c0234759121a926d61f034a833f60
jirka <jorgetimes2@gmail.com>
2025-05-27 21:04:59 +0200

    modfiy leraning rate and other stuff

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@ torchrun --standalone --nproc_per_node=1 train_gpt2.py \
   --input_val_bin "data/fineweb10B/fineweb_val_*.bin" \
   --output_dir pylog124M \
   --model d12 \
-  --batch_size 16 \
-  --grad_accumulation_steps 32 \
-  --sequence_length 1024 \
+  --batch_size 4 \
+  --grad_accumulation_steps 256 \
+  --sequence_length 512 \
   --val_loss_every 128 \
   --val_batch_size 16 \
   --num_iterations 4768 \
   --weight_decay 0.1 \
-  --learning_rate 0.0018 \
+  --learning_rate 1.8e-4 \
   --warmup_iters 256 \
   --warmdown_iters 1024 \
   --log_wandb
